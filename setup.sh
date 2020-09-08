88a58e3d98c4ee127c25894ea42d2afb2506a077
Andy English <andye456@gmail.com>
2020-09-08 20:30:48 +0100

    setup updated

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Change the target address from localhost to AWS IP address
-AWS_IP="35.176.56.125"
 sed -i s/localhost/35.176.56.125/ Sender/RPiData.py
 # Change the webserver address to 0.0.0.0
 sed -i s/localhost/0.0.0.0/ Receiver/Receiver.py
 # Change the requirements to ref the correct version of RPi.GPIO
-sed s/.*GPIO.*/RPi.GPIO==0.7.0/ requirements.txt
+sed -i s/.*GPIO.*/RPi.GPIO==0.7.0/ requirements.txt
 # Change the test temp file to the real one
 sed -i s/.*Temp_File.*/Temp_File = "/sys/bus/w1/devices/28-01193c3b3149/w1_slave"/ Sender/RPiData.py
 # Check the version of python avilable
