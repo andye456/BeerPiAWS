52d35486f6170b818e4ea25a1801eab43da53216
Andy English <andye456@gmail.com>
2020-09-08 21:06:46 +0100

    setup updated

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sed -i s/localhost/0.0.0.0/ Receiver/Receiver.py
 # Change the requirements to ref the correct version of RPi.GPIO
 sed -i s/.*GPIO.*/RPi.GPIO==0.7.0/ requirements.txt
 # Change the test temp file to the real one
-sed -i 's/.*Temp_File.*/Temp_File = "\/sys\/bus\/w1\/devices\/28-01193c3b3149\/w1_slave"/' Sender/RPiData.py
+sed -i 's/w1_slave/\/sys\/bus\/w1\/devices\/28-01193c3b3149\/w1_slave/' Sender/RPiData.py
 # Check the version of python avilable
 echo `which python3`
 # create the virtual env
@@ -20,4 +20,10 @@ else
 fi
 echo "Switching to venv"
 . venv/bin/activate
-[[ $? -eq 0 ]] && pip install -r requirements.txt || exit 1
+if [[ $? -eq 0 ]]
+then
+  pip install -r requirements.txt
+else
+  echo "something went wrong"
+  exit 1
+fi
