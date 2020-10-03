0f50cfeddd79c85b2b58cdac89c95ed1d14a9d2d
Andy English <andye456@gmail.com>
2020-10-03 13:35:29 +0100

    typo in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,9 @@ sed -i s/.*GPIO.*/RPi.GPIO==0.7.0/ requirements.txt
 sed -i 's/w1_slave1/\/sys\/bus\/w1\/devices\/28-01193c3b3149\/w1_slave/' Sender/SocketClient.py
 sed -i 's/w1_slave2/\/sys\/bus\/w1\/devices\/28-01193c5f3e7f\/w1_slave/' Sender/SocketClient.py
 # change the localhost int he html to pevent CORS error
-sed -i s/localhost/35.176.56.125/ Receiver/templates/beer.html
+sed -i s/localhost/35.176.56.125/ beer.html
+# Copy the html to htdocs
+cp beer.html ~/htdocs
 # Check the version of python avilable
 echo `which python3`
 # create the virtual env
